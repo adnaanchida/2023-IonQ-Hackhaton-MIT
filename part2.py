528068d7d9a9c11d48ddd3a6d40fe5c275c1f1f7
adnaanchida <64286274+adnaanchida@users.noreply.github.com>
2023-01-28 15:34:15 -0600

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,39 +1,6 @@
 # Adnaan Chida
 # adnaan.chida@gmail.com
 
-# part 1
-
-n=len(dataset)
-mse=0
-gatecount=0
-
-# Functions 'encode' and 'decode' are dummy.
-def encode(image):
-    q = qiskit.QuantumRegister(3)
-    circuit = qiskit.QuantumCircuit(q)
-    if image[0][0]==0:
-        circuit.rx(np.pi,0)
-    return circuit
-
-def decode(histogram):
-    if 1 in histogram.keys():
-        image=[[0,0],[0,0]]
-    else:
-        image=[[1,1],[1,1]]
-    return image
-
-def run_part1(image):
-    #encode image into a circuit
-    circuit=encode(image)
-
-    #simulate circuit
-    histogram=simulate(circuit)
-
-    #reconstruct the image
-    image_re=decode(histogram)
-
-    return circuit,image_re
-
 # part 2
 
 def encode(image):
@@ -62,5 +29,6 @@ def run_part2(image):
     #convert histogram to category
     label=histogram_to_category(histogram)
         
+    # return label and circuit
     return circuit
     return label
